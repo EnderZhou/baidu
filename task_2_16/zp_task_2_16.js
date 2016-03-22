09286423ad2b7808944a97f84c18a4e56555826f
EnderZhou <zptxwd@gmail.com>
2016-03-22 23:19:08 +0800

    update task_2_16

---
+++ task_2_16/zp_task_2_16.js
@@ -0,0 +1,61 @@
+/**
+ * aqiData，存储用户输入的空气指数数据
+ * 示例格式：
+ * aqiData = {
+ *    "北京": 90,
+ *    "上海": 40
+ * };
+ */
+var aqiData = {};
+
+/**
+ * 从用户输入中获取数据，向aqiData中增加一条数据
+ * 然后渲染aqi-list列表，增加新增的数据
+ */
+function addAqiData() {
+	var city = document.getElementById("aqi-city-input").value;
+	var value = document.getElementById("aqi-value-input").value;
+	// 缺少字符串验证相关代码 待补完
+	aqiData[city] = Number(value);
+}
+
+/**
+ * 渲染aqi-table表格
+ */
+function renderAqiList() {
+	var html = '<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>'
+	var elm = document.getElementById("aqi-table");
+	for(x in aqiData){
+		html += '<tr><td>'+x+'</td><td>'+aqiData[x]+'</td><td><button>删除</button></td></tr>'
+	}
+	elm.innerHTML = html;
+}
+
+/**
+ * 点击add-btn时的处理逻辑
+ * 获取用户输入，更新数据，并进行页面呈现的更新
+ */
+function addBtnHandle() {
+  addAqiData();
+  renderAqiList();
+}
+
+/**
+ * 点击各个删除按钮的时候的处理逻辑
+ * 获取哪个城市数据被删，删除数据，更新表格显示
+ */
+function delBtnHandle() {
+  // do sth.
+
+  renderAqiList();
+}
+
+function init() {
+  // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
+  var addbtn = document.getElementById("add-btn");
+  addbtn.addEventListener("click",addBtnHandle,false);
+  // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+
+}
+
+init();
